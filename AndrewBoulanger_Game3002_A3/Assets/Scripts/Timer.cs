d64e022e4438a96420d58abf3f22057e97531f30
Andrew Boulanger <andrew_boulanger14@hotmail.com>
2021-04-19 18:39:15 -0400

    Added timer, pauses game when it ends

---
+++ AndrewBoulanger_Game3002_A3/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float timeLeft = 10f;
+    [SerializeField] private Text timerText = null;
+    [SerializeField] private Text GameOverText = null;
+    [SerializeField] private Text InstructionsText = null;
+    private string displayString;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        
+        timerText.text = string.Format("Time: {0}.{1:00}",((int)timeLeft /60), ((int)timeLeft%60));
+        if (timeLeft <= 0.0f)
+        {
+            Time.timeScale = 0;
+            timerText.text = string.Format("Time: 0.00");
+            GameOverText.text = "time";
+            InstructionsText.text = "press Q to reset";
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Time.timeScale = 1;
+                SceneManager.LoadScene("game");
+            }
+        }
+    }
+}
