f73149d3cab921b674e1ceea2e8cc1001b174bee
Andrew Boulanger <andrew_boulanger14@hotmail.com>
2021-04-19 21:36:55 -0400

    added camera following

---
+++ AndrewBoulanger_Game3002_A3/Assets/Scripts/SpringArmCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringArmCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField] public float smoothSpeed = 0.125f;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 EndPosition = target.position + offset;
+        Vector3 LerpedPosition = Vector3.Lerp(transform.position, EndPosition, smoothSpeed);
+
+        transform.position = LerpedPosition;
+    }
+}
